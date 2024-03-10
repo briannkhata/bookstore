44e9ad2573804bb9960d0be06510df5529747e2d
Brian Nkhata <bnkhata@mra.mw>
2024-03-10 14:35:20 +0200

    created model book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=256,null=True)
+    shortDescription = models.TextField(null=True)
+    longDescription = models.TextField(null=True)
+    #isbn = models.CharField(max_length=256,null=True)
+    #publishedDate = models.DateField()
+    #status = models.CharField(max_length=20)
+    #authors = models.ManyToManyField('Author')
+    #categories = models.ManyToManyField('Category')
